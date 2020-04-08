a3eca1ba16bbc306d1a5e0938bc17df39ce929f0
kathiravan B <itsmekathi92@gmail.com>
2020-04-08 15:02:20 +0530

    Add List Types from service

--- app/static/scripts/app/lists/lists.controller.js
+++ app/static/scripts/app/lists/lists.controller.js
@@ -2,34 +2,21 @@
     'use strict';
     angular.module('app')
         .controller('AddNewListController', ['$scope', '$mdDialog',
-            function ($scope, $mdDialog, ListDataService) {
-                var toastr = window.toastr;
-                var self = this;
-                $scope.hide = function () {
-                    $mdDialog.hide();
-                };
+            function ($scope, $mdDialog) {
                 $scope.cancel = function () {
                     $mdDialog.cancel();
                 };
 
                 $scope.save = function () {
-                    self.addNew($scope.list);
-                };
-
-                self.addNew = function (listType) {
-                    ListDataService.addNewListType(listType)
-                        .then(function () {
-                            toastr.success('New status updated', 'Data Saved');
-                            $mdDialog.hide(JSON.stringify($scope.list));
-                        }, function () {
-                            toastr.error('Unable to save data', 'Error');
-                            $mdDialog.hide(JSON.stringify($scope.list));
-                        });
+                    $mdDialog.hide($scope.list);
                 };
             }])
-        .controller('ListsController', ['$scope', '$log', '$mdDialog', 'ListDataService',
-            function ($scope, $log, $mdDialog,ListDataService) {
+        .controller('ListsController', ['$scope', '$log', '$mdDialog','ListDataService',
+            function ($scope, $log, $mdDialog, ListDataService) {
                 $log.log('Lists controller initialized');
+                var self = this;
+                var toastr = window.toastr;
+                $scope.listTypes = [];
 
                 // Angular material dialog
                 $scope.showConfirm = function (ev, itemId) {
@@ -48,7 +35,7 @@
                     });
                 };
 
-                // Add new Expense Type
+                // Add new Expense Type Dialog
                 $scope.showAdvanced = function (ev) {
                     $mdDialog.show({
                         controller: 'AddNewListController',
@@ -57,11 +44,31 @@
                         targetEvent: ev,
                         clickOutsideToClose: true,
                         fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
-                    }).then(function (answer) {
-                        $log.log('New item is added');
+                    }).then(function (listType) {
+                        self.saveListType(listType);
                     }, function () {
-                        $log.log('Cancelled');
+                        $log.log('Cancelled Save');
                     });
                 };
+
+                this.getListTypes = function(){
+                    ListDataService.getListTypeLookups()
+                    .then(function(response){
+                        toastr.success('List type lookups fetched', 'Success');
+                        $scope.listTypes = response.data;
+                    }, function(error){
+                        toastr.error('Failed to fetch List Types', 'Error');
+                        $log.log('Error: ' + JSON.stringify(error));
+                    })
+                };
+                this.saveListType = function(listType){
+                    ListDataService.addNewListType(listType)
+                        .then(function () {
+                            toastr.success('New type has been updated', 'Data Saved');
+                        }, function () {
+                            toastr.error('Unable to save data', 'Error');
+                        });
+                };
+                this.getListTypes();
             }]);
 })();
