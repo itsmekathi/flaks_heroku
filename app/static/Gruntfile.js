0c5f1f54f5c90c943744d2c543f6a1ba07943c1c
kathiravan B <itsmekathi92@gmail.com>
2020-05-05 09:36:15 +0530

    Implement minification and refactor code

---
+++ app/static/Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        uglify: {
+            options: {
+            },
+            libraries: {
+                files: {
+                    // Bundle all the libraries which are not pointing to cdn.
+                    'scripts/distribution/libraries.min.js':
+                        [
+                            'scripts/distribution/libraries/angular-chart.min.js'
+                        ]
+                }
+            },
+            angular_files: {
+                files: {
+                    // Bundle the module, directives and shared services into one file
+                    'scripts/distribution/app.module.min.js':
+                        [
+                            'scripts/app/app.module.js',
+                            'scripts/app/directives/currentTime.directive.js',
+                            'scripts/app/directives/httprequest.directive.js',
+                            'scripts/app/directives/modal.directive.js',
+                            'scripts/app/sharedservices/modal.service.js',
+                            'scripts/app/sharedservices/toastr.service.js',
+                            'scripts/app/sharedservices/ui.modal.service.js',
+                        ]
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Default task.
+    grunt.registerTask('minify', ['uglify']);
+
+};
--- app/static/scripts/app/sharedservices/ui.modal.service.js
+++ app/static/scripts/app/sharedservices/ui.modal.service.js
@@ -6,20 +6,16 @@
                 var elm;
                 var modal = {
                     open: function () {
-                        var html = `<div class="modal" ng-style="modalStyle">{{modalStyle}}
-                        <div class="modal-dialog">
-                        <div class="modal-content">
-                        <div class="modal-header">
-                        </div><div class="modal-body">
-                        <ng-transclude></ng-transclude>
-                        </div>
-                        </div>
-                        <div class="modal-footer">
-                         <button id="buttonClose" class="btn btn-primary" ng-click="close()">Close</button>
-                        </div>
-                        </div>
-                        </div>
-                        </div>`;
+                        var html = '<div class="modal" ng-style="modalStyle">{{modalStyle}}   \
+                                    <div class="modal-dialog"> \
+                                    <div class="modal-content"> \
+                                    <div class="modal-header"> \
+                                    </div><div class="modal-body"> \
+                                    <ng-transclude></ng-transclude> \
+                                    </div> </div>\
+                                    <div class="modal-footer">\
+                                    <button id="buttonClose" class="btn btn-primary" ng-click="close()">Close</button> \
+                                    </div>  </div>  </div></div>';
                         elm = angular.element(html);
                         angular.element(document.body).prepend(elm);
 
