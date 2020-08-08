949d7b0bbdfdd0ac79afb64c51e2842aa964cc3b
kathiravan B <itsmekathi92@gmail.com>
2020-08-08 19:34:08 +0530

    fix pagination and update index page for journa;

--- app/journal/routes.py
+++ app/journal/routes.py
@@ -1,4 +1,4 @@
-from flask import abort, flash, jsonify, redirect, render_template, url_for, abort
+from flask import abort, flash, jsonify, redirect, render_template, url_for, abort, request
 from flask_login import current_user, login_required
 
 from app import db
@@ -18,9 +18,11 @@ def index():
         db.session.add(personal_journal)
         db.session.commit()
         return redirect(url_for('journal.index'))
-    personal_journals = PersonalJournal.query.order_by(
-        PersonalJournal.created_on.desc()).all()
-    return render_template('journal/index.html', form=form, personal_journals=personal_journals)
+
+    page = request.args.get('page', 1, type=int)
+    pagination = PersonalJournal.query.order_by(
+        PersonalJournal.created_on.desc()).paginate(page, 5, error_out=False)
+    return render_template('journal/index.html', form=form, pagination=pagination)
 
 
 @login_required
@@ -31,3 +33,15 @@ def user(username):
         abort(404)
     journals = user.journals.orderby(PersonalJournal.created_on.desc()).all()
     return render_template('journal/user.html', user=user, journals=journals)
+
+
+@login_required
+@journal.route('/<int:id>')
+def journalbyid(id):
+    return "hellow"
+
+
+@login_required
+@journal.route('/edit/<int:id>')
+def editbyid(id):
+    return "edit"
