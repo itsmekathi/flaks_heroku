cc449e14dc2906544f0bf7de56078c8006f1648b
Thiruveni B <thiruveni.b@kyndryl.com>
2022-12-05 19:53:30 +0530

    remove journal from routing

--- app/journal/__init__.py
+++ app/journal/__init__.py
@@ -1,3 +1,3 @@
-from flask import Blueprint
-journal = Blueprint('journal', __name__)
-from . import routes
+## from flask import Blueprint
+## journal = Blueprint('journal', __name__)
+## from . import routes
